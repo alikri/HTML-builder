448ab7fa2b95593069b5817ee5546bda27659205
Alikri <alikri@gmail.com>
2023-05-05 11:56:28 +0200

    feat: second task complete

---
+++ 02-write-file/index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'favorite.txt');
+const output = fs.createWriteStream(filePath, 'utf-8');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('What kind of junk food do you like?');
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    exitProcess();
+  } else {
+    output.write(`${input}\n`);
+  }
+});
+
+function exitProcess() {
+  console.log('Удачи в изучении Node.js!');
+  output.end();
+  rl.close();
+  process.exit();
+}
+
+rl.on('SIGINT', () => {
+  exitProcess();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const readableStream = fs.createReadStream('source.txt');
+
+
+
+
+
+// fs.mkdir(path.join(__dirname, 'riddle'), err => {
+// 	if (err) throw err;
+// 	console.log('Папка была создана');
+// });
