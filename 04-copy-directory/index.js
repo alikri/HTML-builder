bf9b9467ad472a8059309b0b09bb78e010c077c8
Alikri <alikri@gmail.com>
2023-05-05 15:46:50 +0200

    feat: task 4 complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
 
+const fs = require('fs/promises');
+const path = require('path');
+
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(destDir, { recursive: true })
+  .then(() => fs.readdir(srcDir))
+	.then(files => {
+    const copyPromises = files.map(file =>
+      fs.copyFile(path.join(srcDir, file), path.join(destDir, file))
+    );
+    return Promise.all(copyPromises);
+  })
+  .then(() => {
+    console.log('All files have been copied successfully');
+  })
+  .catch(err => {
+    console.error('An error occurred:', err);
+  });
+
